14ed25d72114c2ac3efe87eb9543ce478b8cfafc
Binotz <binotto.riccardo@gmail.com>
2022-09-05 15:26:13 +0200

    Posts controller and model created

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -15,9 +15,14 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    
                 </div>
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+
+    <div>Benvenuto {{Auth::user()->name}}! </div>
+    
 @endsection
